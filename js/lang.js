59d40201b956c73a952137d83c9cd419d869e4e3
maxim <max280596@gmail.com>
2021-02-25 12:43:07 +0200

    ddddddddddd

---
+++ js/lang.js
@@ -0,0 +1,60 @@
+const langObj = {
+    "unit": {
+        "en" : "Explainer Videos & Animation | Beemloop",
+        "ua" : "Бімлуп",
+    },
+    "home" : {
+        "en": "Home",
+        "ua": "Головна",
+    },
+    "about" : {
+        "en": "About",
+        "ua": "Про нас",
+    },
+    "portfolio" : {
+        "en": "Portfolio",
+        "ua": "Портфоліо",
+    },
+    "packages" : {
+        "en": "Packages",
+        "ua": "Пакети",
+    },
+    "team" : {
+        "en": "Team",
+        "ua": "Команда",
+    },
+    "contact" : {
+        "en" : "Contact Us",
+        "ua" : "Контакти",
+    },
+    "consultation" : {
+        "en" : "Get online consultation",
+        "ua" : "Онлайн-консультацію",
+    },
+    "we" : {
+        "en" : "We explain",
+        "ua" : "Ми пояснюємо",
+    },
+    "you" : {
+        "en" : "your product",
+        "ua" : "ваш товар",
+    },
+    
+    "in" : {
+        "en" : "in 90 seconds",
+        "ua" : "за 90 секунд",
+    },
+    "more" : {
+        "en" : "More than 500 of our clients got their products explained using videos that we created.",
+        "ua" : "Більше 500 наших клієнтів пояснили свої товари за допомогою створених нами відео.",
+    },
+    "br" : {
+        "en" : "In just 90 seconds customer will know everything about your product.",
+        "ua" : "Всього за 90 секунд клієнт дізнається все про ваш продукт.",
+    },
+    "quote" : {
+        "en" : "Get A Quote",
+        "ua" : "Отримати цитату",
+    } 
+    
+};
---
+++ js/translate.js
@@ -0,0 +1,33 @@
+const select = document.querySelector('select');
+const allLang = ['en', 'ua'];
+
+select.addEventListener('change', changeURLLanguage);
+
+function changeURLLanguage() {
+    let lang = select.value;
+    location.href = window.location.pathname + '#' + lang;
+    location.reload();
+}
+
+function changeLanguage() {
+    let hash = window.location.hash;
+    hash = hash.substr(1);
+    console.log(hash)
+    if(!allLang.includes(hash)) {
+        location.href = window.location.pathname + '#en';
+        location.reload();
+    }
+    select.value = hash;
+    document.querySelector('title').innerHTML = langObj['unit'][hash];
+
+    for (let key in langObj) {
+        let elem = document.querySelector('.lng-' + key);
+        if (elem) {
+            elem.innerHTML = langObj[key][hash];
+        }
+
+    }
+}
+
+
+changeLanguage()
